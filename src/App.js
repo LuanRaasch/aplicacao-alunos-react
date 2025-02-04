df7a1bde032adfd4882dee1fa3896de744b339ce
LuanRaasch <luanraasch@outlook.com>
2025-02-04 20:50:32 -0300

    firstcommit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Router } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+
+import GlobalStyles from "./styles/GlobalStyles";
+import Header from "./components/Header";
+import Routes from "./routes";
+import history from "./services/history";
+
+function App() {
+  return (
+    <Router history={history}>
+      <Header />
+      <Routes />
+      <GlobalStyles />
+      <ToastContainer autoClose={3000} className="toast-container" />
+    </ Router>
+  );
+}
+
+export default App;
